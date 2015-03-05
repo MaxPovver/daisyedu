ceb2d9c8b3bcede14ec3379d29562c709bde87a1
MaxPovver <admin@MacBook-Pro-Maksim.local>
2015-03-05 21:11:13 +0300

    Initial Commit

---
+++ daisyedu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  daisyedu
+//
+//  Created by Максим Чистов on 05.03.15.
+//  Copyright (c) 2015 Максим Чистов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
