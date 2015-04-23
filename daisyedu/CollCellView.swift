e2c67a3b6377c468947286f673e8753839bab973
MaxPovver <admin@MacBook-Pro-Maksim.local>
2015-04-23 15:26:15 +0300

    Получше выглядит но тупит

--- daisyedu/CollCellView.swift
+++ daisyedu/CollCellView.swift
@@ -5,16 +5,37 @@
 //  Created by Максим Чистов on 23.04.15.
 //  Copyright (c) 2015 Максим Чистов. All rights reserved.
 //
-
 import UIKit
 
 public class CollCellView: UICollectionViewCell {
 
     @IBOutlet weak var Title: UILabel!
-    @IBOutlet weak var Picture: UIWebView!
+    @IBOutlet weak var Picture: UIImageView!
     public func set(node:TreeNode) {
-        Title.text = node.value.getTitle()
-        let url = "<img src=\"\(node.value.getImage()!)\" width=\"100%\">"
-        Picture.loadHTMLString(url, baseURL: NSURL(string: "http://daisyedu.com/"))
+        dispatch_async(
+            
+            dispatch_get_main_queue(),{
+        self.Title.text = node.value.getTitle()
+        var p = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).last as! String
+        let path = p + "/cache/img/\(node.value.getID())"
+       if NSFileManager.defaultManager().fileExistsAtPath(path)
+       {
+            self.Picture.image = UIImage(contentsOfFile: path)
+       } else {
+        var request =  node.value.getImage()!
+        if !(request.hasPrefix("http") ) {
+            request = "http://daisyedu.com/" + request
+        }
+        var data = NSData(contentsOfURL: NSURL(string:request.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!)
+         self.Picture.image = UIImage(data: data!)
+        dispatch_async(
+            
+                dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {autoreleasepool {data?.writeToFile(path, atomically: true)
+                    data = nil
+                }
+        })
+        }
+        })
+        
     }
 }
